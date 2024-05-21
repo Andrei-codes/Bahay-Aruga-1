425bbde97206a6bb980c3d0d457ccbf8456d5b01
Francis Cabusas <117327118+FrancisPogi22@users.noreply.github.com>
2024-05-22 05:59:10 +0800

    Initialized System

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, session, request
+from model import db, Users
+
+app = Flask(__name__)
+app.secret_key = "kagaguhan"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///sqlite.db"
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register-user', methods=['POST', 'GET'])
+def register_user():
+    name, email, password, password2 = request.form['name'], request.form['email'], request.form['password'], request.form['password_2']
+    if not (name and email and password and password2):
+        return "fill out all fields", 400
+    if password != password2:
+        return "password not matched", 400
+    user_entry = Users(
+        name=name,
+        email=email,
+        password=password
+    )
+    db.session.add(user_entry)
+    db.session.commit()
+    return "true", 200
+
+@app.route('/auth-user', methods=['POST', 'GET'])
+def auth_user():
+    email, password = request.form['email'], request.form['password']
+    if not (email and password):
+        return "fill out all fields", 400
+    if not Users.auth_user(email, password):
+        return "false", 400
+    return "true", 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    @classmethod
+    def auth_user(cls, email, password):
+        user = cls.query.filter_by(email=email).first()
+        if not user:
+            return False
+        if not user.password==password:
+            return False
+        return True
